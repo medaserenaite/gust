7638660997f79b08c74b8a5337423bc27cd34b3e
Meda <meda.serenaite@gmail.com>
2019-01-24 22:57:32 -0600

    added Angular project

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -17,13 +17,19 @@ namespace gust.Controllers
         {
             _context = context;
         }
-        // GET api/values
-        // [HttpGet]
-        // public IActionResult AllAirports()
-        // {
-        //     var airports = _context.Airports.ToList();
-        //     return Ok(airports);
-        // }
+
+        //------------------------------ALL AIRPORTS------------------
+        //---using this, when choosing an airport
+        [Route("/allairports")]
+        [HttpGet]
+        public IActionResult AllAirports()
+        {
+            var airports = _context.Airports.ToList();
+            return Ok(airports);
+        }
+
+        //-------------------------------ALL RUNWAYS-----------------
+        [Route("/allrunways")]
         [HttpGet]
         public IActionResult AllRunways()
         {
@@ -31,14 +37,38 @@ namespace gust.Controllers
             return Ok(runways);
         }
 
+        // ---------------------------LONG AND LAT OF THE CHOSEN AIRPORT----
+        [Route("/{air_id}")]
+        [HttpGet]
+        public IActionResult ChosenAirport(int air_id)
+        {
+            var chosenAirport = _context.Airports.FirstOrDefault(ca=>ca.airport_id == air_id);
+            return Ok(chosenAirport);       
+        }
+
+        //uses longitude and latitude from above to connect to weather api in the front end
+        //finds weather in the location
+        //we pull that location's wind's angle
+        //compare wind's angle to runway angles
+        //-----more steps to compare all the runways?
+        //when
+
+
+
+
+
         [HttpGet("{run_id}")]
         public IActionResult OneRunwayDeg(int run_id)
         {
             var runway = _context.Runways.FirstOrDefault(x=>x.runway_id == run_id);
             // var runwaydeg = _context.Runways.Where
             Runway thisRunway = _context.Runways.FirstOrDefault(c=>c.runway_id == run_id);
-            return Ok(thisRunway);
+            return Ok(thisRunway.airport_ref);
         }
+
+
+
+
         // GET api/values/5
         // [HttpGet("{airport_id}")]
         // public ActionResult<string> Get(int id)
